6d306a6c6de14c866edd12c015fbefb4a4b4a43e
Carl Reed <carl.w.reed@vumc.org>
2019-06-04 14:08:20 -0500

    update master.php

--- master.php
+++ master.php
@@ -2,13 +2,57 @@
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
 
 require "libs/PhpSpreadsheet/vendor/autoload.php";
+require_once "libs/PhpSpreadsheet/src/Bootstrap.php";
 
+use PhpOffice\PhpSpreadsheet\Helper\Sample;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+$helper = new Sample();
+
+// Create new Spreadsheet object
 $spreadsheet = new Spreadsheet();
-$sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', 'Hello World !');
 
-$writer = new Xlsx($spreadsheet);
-$writer->save("hello world.xlsx");
+// Set document properties
+$spreadsheet->getProperties()->setCreator('Maarten Balliauw')
+    ->setLastModifiedBy('Maarten Balliauw')
+    ->setTitle('Office 2007 XLSX Test Document')
+    ->setSubject('Office 2007 XLSX Test Document')
+    ->setDescription('Test document for Office 2007 XLSX, generated using PHP classes.')
+    ->setKeywords('office 2007 openxml php')
+    ->setCategory('Test result file');
+
+// Add some data
+$spreadsheet->setActiveSheetIndex(0)
+    ->setCellValue('A1', 'Hello')
+    ->setCellValue('B2', 'world!')
+    ->setCellValue('C1', 'Hello')
+    ->setCellValue('D2', 'world!');
+
+// Miscellaneous glyphs, UTF-8
+$spreadsheet->setActiveSheetIndex(0)
+    ->setCellValue('A4', 'Miscellaneous glyphs')
+    ->setCellValue('A5', 'éàèùâêîôûëïüÿäöüç');
+
+// Rename worksheet
+$spreadsheet->getActiveSheet()->setTitle('Simple');
+
+// Set active sheet index to the first sheet, so Excel opens this as the first sheet
+$spreadsheet->setActiveSheetIndex(0);
+
+// Redirect output to a client’s web browser (Xlsx)
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="01simple.xlsx"');
+header('Cache-Control: max-age=0');
+// If you're serving to IE 9, then the following may be needed
+// header('Cache-Control: max-age=1');
+
+// If you're serving to IE over SSL, then the following may be needed
+// header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+// header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
+// header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+// header('Pragma: public'); // HTTP/1.0
+
+$writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+$writer->save('php://output');
+// exit;
