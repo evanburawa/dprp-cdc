7a9b770f0006dd974ce258f0608f2e0dfcdc0b42
Carl Reed <carl.w.reed@vumc.org>
2019-06-04 15:13:33 -0500

    temp adding sample and record import files

--- master.php
+++ master.php
@@ -4,55 +4,52 @@ $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
 require "libs/PhpSpreadsheet/vendor/autoload.php";
 require_once "libs/PhpSpreadsheet/src/Bootstrap.php";
 
-use PhpOffice\PhpSpreadsheet\Helper\Sample;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
-$helper = new Sample();
+// make DPP file
+$spreadsheet = IOFactory::load("DPPMasterTemplate.xlsx");
 
-// Create new Spreadsheet object
-$spreadsheet = new Spreadsheet();
+// move ave/wt loss/goal area temporarily
+$weightLosses = $spreadsheet->getActiveSheet()
+	->rangeToArray(
+		"A11:D15",
+		NULL,
+		TRUE,
+		TRUE,
+		TRUE
+	);
 
-// Set document properties
-$spreadsheet->getProperties()->setCreator('Maarten Balliauw')
-    ->setLastModifiedBy('Maarten Balliauw')
-    ->setTitle('Office 2007 XLSX Test Document')
-    ->setSubject('Office 2007 XLSX Test Document')
-    ->setDescription('Test document for Office 2007 XLSX, generated using PHP classes.')
-    ->setKeywords('office 2007 openxml php')
-    ->setCategory('Test result file');
+//test weightLosses set
+$spreadsheet->getActiveSheet()
+	->fromArray(
+		$weightLosses,
+		NULL,
+		"A19"
+	);
+$styleBold = [
+	"font" => [
+		"bold" => true
+	]
+];
+$spreadsheet->getActiveSheet()->getStyle("A19:A23")->applyFromArray($styleBold);
 
-// Add some data
-$spreadsheet->setActiveSheetIndex(0)
-    ->setCellValue('A1', 'Hello')
-    ->setCellValue('B2', 'world!')
-    ->setCellValue('C1', 'Hello')
-    ->setCellValue('D2', 'world!');
-
-// Miscellaneous glyphs, UTF-8
-$spreadsheet->setActiveSheetIndex(0)
-    ->setCellValue('A4', 'Miscellaneous glyphs')
-    ->setCellValue('A5', 'éàèùâêîôûëïüÿäöüç');
-
-// Rename worksheet
-$spreadsheet->getActiveSheet()->setTitle('Simple');
+// rename worksheets
+// $spreadsheet->getActiveSheet()->setTitle('Combined');
 
 // Set active sheet index to the first sheet, so Excel opens this as the first sheet
 $spreadsheet->setActiveSheetIndex(0);
 
 // Redirect output to a client’s web browser (Xlsx)
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="01simple.xlsx"');
+header('Content-Disposition: attachment;filename="DPP Participant Sessions.xlsx"');
 header('Cache-Control: max-age=0');
-// If you're serving to IE 9, then the following may be needed
-// header('Cache-Control: max-age=1');
 
-// If you're serving to IE over SSL, then the following may be needed
+// // If you're serving to IE over SSL, then the following may be needed
 // header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
 // header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
-// header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+// header('Cache-Control: cache, must-revalidate, max-age=1'); // HTTP/1.1
 // header('Pragma: public'); // HTTP/1.0
 
 $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
 $writer->save('php://output');
-// exit;
