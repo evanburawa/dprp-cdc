7f48a92ce2d17bc435c1e9fb643ef9dc67533a69
Carl Reed <carl.w.reed@vumc.org>
2019-09-12 10:40:02 -0500

    coach-cohort workbook changes

--- coach_cohort_page.php
+++ coach_cohort_page.php
@@ -4,17 +4,26 @@ require_once('config.php');
 require_once "../../redcap_connect.php";
 require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
 
+$project = new \Project((int) $_GET["pid"]);
+
 // // debug printing
-file_put_contents("C:/vumc/log.txt", print_r($project->metadata["coach_name"]["element_enum"], true) . "\n\n");
+// file_put_contents("C:/vumc/log.txt", print_r($project->metadata["coach_name"]["element_enum"], true) . "\n\n");
 
 // query for potential coach and cohort values
-$project = new \Project((int) $_GET["pid"]);
 preg_match_all("/(\d+),?\s?(.+?)(?=\x{005c}\x{006E}|$)/", $project->metadata["coach_name"]["element_enum"], $matches);
-$coaches = $matches[2];
+$coaches = array_map("trim", $matches[2]);
 preg_match_all("/(\d+),?\s?(.+?)(?=\x{005c}\x{006E}|$)/", $project->metadata["cohort"]["element_enum"], $matches);
 $cohorts = $matches[2];
-file_put_contents("C:/vumc/log.txt", print_r($coaches, true) . "\n\n", FILE_APPEND);
-file_put_contents("C:/vumc/log.txt", print_r($cohorts, true) . "\n\n", FILE_APPEND);
+// file_put_contents("C:/vumc/log.txt", print_r($coaches, true) . "\n\n", FILE_APPEND);
+// file_put_contents("C:/vumc/log.txt", print_r($cohorts, true) . "\n\n", FILE_APPEND);
+
+
+// build list of which cohorts which coaches belong / have belonged to
+// $records = \REDCap::getData((int) $_GET['pid'], 'array', null, ['coach_name, cohort']);
+// foreach ($records as $rid => $record) {
+	// file_put_contents("C:/vumc/log.txt", print_r($records, true) . "\n\n");
+	
+// }
 
 // create dropdowns
 $coach_dd = "
@@ -30,7 +39,7 @@ foreach ($coaches as $i => $name) {
 }
 $coach_dd .= "
 			</div>
-		</div>";
+		</div><br/>";
 		
 $cohort_dd = "
 		<div class='dropdown'>
@@ -45,7 +54,7 @@ foreach ($cohorts as $i => $name) {
 }
 $cohort_dd .= "
 			</div>
-		</div>";
+		</div><br/>";
 
 $ccwb_js = file_get_contents("js/ccwb.js");
 $html = "
@@ -55,7 +64,9 @@ $html = "
 	$coach_dd
 	<h5>Select Cohorts to Include:</h5>
 	$cohort_dd
-	<button id='generateWorkbook' class='btn btn-primary' style='margin: 32px 80px'>Generate Coach-Cohort Workbook</button>
+	<br/>
+	<p style='display: none' id='ajaxInfo'></p>
+	<button id='generateWorkbook' class='btn btn-primary' disabled=true style='margin: 32px 80px'>Generate Coach-Cohort Workbook</button>
 ";
 
 echo $html;
--- js/ccwb.js
+++ js/ccwb.js
@@ -1,17 +1,48 @@
 // on document ready
-$(document).ready(
-	
-);
+var DPRP = {};
 
 $('body').on('click', '#generateWorkbook', function() {
-	
+	if (DPRP.cohort && DPRP.coach && pid) {
+		let url = "master.php?pid=" + pid + "&coach=" + DPRP.coach + "&cohort=" + DPRP.cohort;
+		url = encodeURI(url);
+		window.open(url);
+	}
 });
 
 $('body').on('click', '.dropdown-menu a', function() {
 	let dd = $(this).parent().siblings("button");
-	console.log(dd);
 	dd.text($(this).text());
 	$(".btn:first-child").val($(this).text());
+	
+	if (dd[0] == $("#coachDropdown")[0]) {
+		DPRP.coach = dd.text();
+	} else if (dd[0] == $("#cohortDropdown")[0]) {
+		DPRP.cohort = dd.text();
+	}
+	
+	let pid = getUrlParameter("pid");
+	if (DPRP.cohort && DPRP.coach && pid) {
+		// show user record count for this coach-cohort
+		let url = encodeURI(`coach_cohort_ajax.php?pid=${pid}&coach=${DPRP.coach}&cohort=${DPRP.cohort}`);
+		$.get({
+			url: url,
+			dataType: "json",
+		}).done(function(response) {
+			console.log('response', response);
+			if (response.recordCount || response.recordCount === 0) {
+				$("#generateWorkbook").attr('disabled', response.recordCount === 0);
+				$("#ajaxInfo").text("Records found for this coach-cohort combination: " + response.recordCount);
+				$("#ajaxInfo").show();
+			} else {
+				$("#generateWorkbook").attr('disabled', true);
+				$("#ajaxInfo").text(response.error);
+				$("#ajaxInfo").show();
+			}
+		});
+	} else {
+		$("#ajaxInfo").hide();
+		$("#generateWorkbook").attr('disabled', true);
+	}
 });
 
 // thanks: https://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
